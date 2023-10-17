1514d5343c8b661e8711722d664291327c99a697
Genteel Magpie <genteelmagpie@gmail.com>
2023-10-17 19:36:28 +0530

    fixed the error related with database file.

--- server.py
+++ server.py
@@ -8,19 +8,17 @@ app = Flask(__name__)
 def home_page():
     return render_template("./index.html")
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
-
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
         retVal = writeToCSV(data)
-        print(retVal)
         if retVal == 1:
             return ' Thank you for reaching out. We will get in touch shortly.'
         elif retVal == -1:
